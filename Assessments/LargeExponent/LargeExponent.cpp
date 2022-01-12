adfe05a2eab8599fb5776c9fbc1dbc5bd850178a
Dogukan Arat <aratdogukan@gmail.com>
2022-01-12 20:15:14 +0300

    Add the required comments to the BigExponent library

--- Assessments/LargeExponent/LargeExponent.cpp
+++ Assessments/LargeExponent/LargeExponent.cpp
@@ -6,7 +6,9 @@ LargeExponent::LargeExponent()
     : m_counter{0}
     , m_base{0}
     , m_power{0}
-{}
+{
+    // Nothing to do here
+}
 
 bool LargeExponent::isDigit(std::string input)
 {
@@ -14,10 +16,12 @@ bool LargeExponent::isDigit(std::string input)
 
     if (input.empty())
     {
+        // if the input is empty, it is an error
         result =  false;
     }
     else
     {
+        // if the each character of the input is a digit, the string is all digit
         result = std::all_of(input.begin(), input.end(), [](char c)
         {
             return c >= '0' && c <= '9';
@@ -28,8 +32,10 @@ bool LargeExponent::isDigit(std::string input)
 
 std::string LargeExponent::reverse(std::string input)
 {
+    // copy of the given input to the temporary string
     std::string result = input;
 
+    // reverse the string
     std::reverse(result.begin(), result.end());
 
     return result;
@@ -39,15 +45,25 @@ bool LargeExponent::input(std::string input)
 {
     bool result = false;
 
-    if( isDigit(input))
+    if(isDigit(input))
     {
+        // convert the input to integer
         int tempInput = std::stoi(input);
 
+        // if the input is a positive integer and 
+        // within the given range, which is given in 
+        // the problem statement
         if(tempInput > 0 && tempInput <= 99999)
         {   
+            // copy the input to the member variable
             m_input = input;
+
+            // set the base
             m_base = tempInput;
+
+            // set the power to reversed input
             m_power = std::stoi(reverse(input));
+
             result = true;
         }
         else
@@ -65,14 +81,19 @@ bool LargeExponent::input(std::string input)
 
 void LargeExponent::initialiseBuffer()
 {
+    // copy the base to the temporary variable
     int tempBase = m_base;
 
     for(int index = 0; tempBase != 0; index++)
     {
+        // push the each digit of the base to the buffer
         m_buffer.push_back(tempBase % 10);
+
+        // find the rest of digit of the base
         tempBase = tempBase / 10;
     }
 
+    // set the counter to the size of the buffer
     m_counter = m_buffer.size();
 }
 
@@ -82,16 +103,20 @@ void LargeExponent::finaliseBuffer()
 
     for (auto it = m_buffer.rbegin(); it != m_buffer.rend(); ++it)
     {
+        // append the each digit of the buffer to the result string
         streamResult.append(std::to_string(*it));
     }
 
+    // copy the result string to the result member variable
     m_result = streamResult;
 }
 
 void LargeExponent::calculateBuffer()
 {
+    // @note : counter is started from 2 since the buffer is already initialised
     for(int counter = 2; counter <= m_power; counter++)
     {
+        // multiply the buffer with the buffer itself time base
         m_counter = multiply(m_base, m_buffer, m_counter);
     }
 }
@@ -100,12 +125,16 @@ bool LargeExponent::calculate()
 {
     bool result = false;
 
+    // if the base valid
     if(m_base > 0)
     {
+        // initialise the buffer
         initialiseBuffer();
 
+        // do the multiplication
         calculateBuffer();
 
+        // set the output
         finaliseBuffer();
 
         result = true;
@@ -124,6 +153,7 @@ bool LargeExponent::output(std::string& output)
 
     if(m_buffer.size() > 0)
     {
+        // copy the result to the output
         output = m_result;
         result = true;
     }
@@ -137,28 +167,37 @@ bool LargeExponent::output(std::string& output)
 
 int LargeExponent::multiply(int base, std::vector<int>& buffer, int size)
 {
+    // note : this function is simply an implementation of paper and pencil method
     int carry = 0;
 
     std::vector<int> resultBuffer;
 
     for (int index = 0; index < size; index++)
     {
+        // multiply the base with the digit of the buffer and add previous carry
         int product = buffer[index] * base + carry;
     
+        // set the product of module 10 to the result buffer
         resultBuffer.push_back(product % 10);
     
+        // find the carry for next iteration
         carry = product / 10;
     }
  
+    // if the carry is still there
     while (carry)
     {
+        // add the carry to the result buffer
         resultBuffer.push_back(carry % 10);
 
+        // find the carry for next iteration
         carry = carry / 10;
 
         size++;
     }
 
+    // set the result buffer to output buffer with assign method
+    // since the result might have different size than the buffer
     buffer.assign(resultBuffer.begin(), resultBuffer.end());
 
     return size;
--- Assessments/LargeExponent/LargeExponent/LargeExponent.h
+++ Assessments/LargeExponent/LargeExponent/LargeExponent.h
@@ -11,26 +11,119 @@ namespace RocketLab
     class LargeExponent
     {
     public:
+        /**
+         * @brief Construct a new Large Exponent object
+         * 
+         */
         LargeExponent();
+
+        /**
+         * @brief Destroy the Large Exponent object
+         * 
+         */
         virtual ~LargeExponent() = default;
 
+        /**
+         * @brief Check if the input is valid
+         * 
+         * @param input : input string
+         * @return true if the input is valid otherwise false
+         */
         bool input(std::string input);
+
+        /**
+         * @brief Set the output string with the answer
+         * 
+         * @return true if the output is ready otherwise false
+         */
         bool output(std::string& output);
+
+        /**
+         * @brief Do the calculation
+         * 
+         * @return true if the calculation succeed otherwise false
+         */
         bool calculate();
 
     protected:
+        /**
+         * @brief Initialise the buffer with the given input string
+         * 
+         */
         void initialiseBuffer();
+
+        /**
+         * @brief Finalise the buffer setting the output string
+         * 
+         */
         void finaliseBuffer();
+
+        /**
+         * @brief Finalise the buffer setting the output string
+         * 
+         */
         void calculateBuffer();
+
+        /**
+         * @brief Multiply the given base with the given buffer
+         * 
+         * @param base : base to multiply
+         * @param buffer : buffer to multiply
+         * @param counter : counter of the buffer
+         * @return int : counter of the buffer
+         */
         int multiply(int base, std::vector<int>& buffer, int size);
+
+        /**
+         * @brief Check if the input parameter is all digits
+         * 
+         * @param input : input string
+         * @return true if the input is all digit otherwise false
+         */
         bool isDigit(std::string input);
+
+        /**
+         * @brief Reverse the given input parameter
+         * 
+         * @param input : input string
+         * @return string : reversed input string
+         */
         std::string reverse(std::string input);
 
+        /**
+         * @brief User input string
+         * 
+         */
         std::string m_input;
+
+        /**
+         * @brief Result of multiplication buffer
+         * 
+         */
         std::vector<int> m_buffer;
+
+        /**
+         * @brief Result string
+         * 
+         */
         std::string m_result;
+
+        /**
+         * @brief Counter of the buffer
+         * 
+         */
         int m_counter;
+
+        /**
+         * @brief Base of exponent
+         * 
+         */
         int m_base;
+
+        /**
+         * @brief power of exponent
+         * 
+         */
         int m_power;
     };
 }
